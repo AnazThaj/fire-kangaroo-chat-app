a1721071594efbcb1ade9502bbeadb0daff90c85
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-24 22:48:52 +0530

    Update the pull

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { connectDB } = require("./db")
+const { connectDB } = require("./db");
 
 // routes
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
 
-dotenv.config()
-const app = express()
-app.use(express.json())
+dotenv.config();
+const app = express();
+app.use(express.json());
 connectDB();
 
 app.get("/", (req, res) => {
-    res.send("Hello")
-})
+  res.send("Hello");
+});
 
-app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server started at port ${process.env.PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started at port ${process.env.PORT}`));
